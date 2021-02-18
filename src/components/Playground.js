b6603f78d77a0edbdaf9da3b9d8315ab3f5502c0
Faisal Akhlaq <faisal.akhlaq@aol.com>
2021-02-18 23:46:45 +0100

    Weather app

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,6 +1,7 @@
 import { Link, BrowserRouter, Route, Switch } from "react-router-dom";
 import Todo from "./Todo";
 import CodilityLessons from "./CodilityLessons"
+import Weather from './Weather'
 
 const Playground = () => {
     return (
@@ -8,10 +9,12 @@ const Playground = () => {
         <div className='base-container'>
             <h1>Small Apps</h1>
             <li><Link className="" to="/todo">Todo App</Link></li>
+            <li><Link className='' to="/weather">Weather</Link></li>
             <li><Link className='' to="/codilitylessons">Codility Lessons</Link></li>
             <hr />
 
             <Switch>
+                <Route path="/weather" component={Weather}/>
                 <Route path="/todo" component={Todo}/>
                 <Route path="/codilitylessons" component={CodilityLessons}/>
             </Switch>
